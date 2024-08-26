552eb21015edd6e353b8f339025ef3915ebe8945
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-08-26 20:26:22 +0330

    develop box blog :)

--- src/Components/Icons/Icons.jsx
+++ src/Components/Icons/Icons.jsx
@@ -70,8 +70,15 @@ const Icons = () => {
                 </symbol>
 
                 <symbol
-                id='xMark' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    id='xMark' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </symbol>
+                <symbol
+                    id='share' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                </symbol>
+                <symbol id='list' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                 </symbol>
 
             </svg>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import BoxBlog from '../../Components/BoxBlog/BoxBlog'
+import ListBlog from './Components/ListBlog'
 
 const Home = () => {
     return (
         <>
             <div className="container">
                 <Header />
+                <ListBlog />
             </div>
         </>
     )
