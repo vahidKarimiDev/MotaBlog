8909475f2bca32fd17563ec842d03107fb5300ae
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-08-26 19:33:34 +0330

    develope Header :)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Icons from './Components/Icons/Icons';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  {/* <App /> */ }
+  <BrowserRouter>
+    <Icons />
+    <App />
+  </BrowserRouter>
 )
