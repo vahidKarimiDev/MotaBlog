d2f3a9295659471fe14d0d2452b4afc46bd98911
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-08-27 12:18:24 +0330

    develop dark mode :)

---
+++ src/Components/BtnDarkMode/BtnDarkMode.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const BtnDarkMode = () => {
+
+    const darkModeHandler = () => {
+        document.documentElement.classList.toggle('dark')
+    }
+
+    return (
+        <>
+            <div className="wrapper-btn__dark">
+                <input type="checkbox" id='dark-mode' onChange={darkModeHandler} />
+                <label className='labelDarkMode' htmlFor="dark-mode">
+                    <div className="wrapper-icon">
+                        <svg className='svg sun'>
+                            <use xlinkHref='#sun'></use>
+                        </svg>
+                        <svg className='svg moon'>
+                            <use xlinkHref='#moon'></use>
+                        </svg>
+
+                    </div>
+                </label>
+            </div>
+        </>
+    )
+}
+
+export default BtnDarkMode
